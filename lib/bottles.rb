ddde42b682f279bd5b3ae8340c87bbf43dd2bb83
Chloe <chloe.wong@madetech.com>
2020-10-08 16:15:59 +0100

    refactor to make num_of_bottles > 2 more general

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,16 +1,10 @@
 class Bottles
     def verse(num_of_bottles)
-        if num_of_bottles == 99
-            return "99 bottles of beer on the wall, " +
-            "99 bottles of beer.\n" +
-            "Take one down and pass it around, " +
-            "98 bottles of beer on the wall.\n"
-        end
-        if num_of_bottles == 3
-            return "3 bottles of beer on the wall, " +
-            "3 bottles of beer.\n" +
-            "Take one down and pass it around, " +
-            "2 bottles of beer on the wall.\n"
+        if num_of_bottles == 1
+            return "1 bottle of beer on the wall, " +
+            "1 bottle of beer.\n" +
+            "Take it down and pass it around, " +
+            "no more bottles of beer on the wall.\n"
         end
         if num_of_bottles == 2
             return "2 bottles of beer on the wall, " +
@@ -18,5 +12,9 @@ class Bottles
             "Take one down and pass it around, " +
             "1 bottle of beer on the wall.\n"
         end
+        "#{num_of_bottles} bottles of beer on the wall, " +
+        "#{num_of_bottles} bottles of beer.\n" +
+        "Take one down and pass it around, " +
+        "#{num_of_bottles - 1} bottles of beer on the wall.\n"
     end
 end
