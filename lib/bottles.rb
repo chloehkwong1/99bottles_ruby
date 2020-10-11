62216e8d085dc1385e027ad61ba6ef9080fa4bdb
Chloe <chloe.wong@madetech.com>
2020-10-11 21:16:59 +0100

    new method container created to facilitate open/closed principle

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,4 +1,12 @@
 class Bottles
+    def song
+        verses(99,0)
+    end
+
+    def verses(upper, lower)
+        upper.downto(lower).collect { |i| verse(i) }.join("\n")
+    end
+
     def verse(number)
         case number
         when 0
@@ -11,24 +19,19 @@ class Bottles
             "1 bottle of beer.\n" +
             "Take it down and pass it around, " +
             "no more bottles of beer on the wall.\n"
-        when 2
-            "2 bottles of beer on the wall, " +
-            "2 bottles of beer.\n" +
-            "Take one down and pass it around, " +
-            "1 bottle of beer on the wall.\n"
         else
             "#{number} bottles of beer on the wall, " +
             "#{number} bottles of beer.\n" +
             "Take one down and pass it around, " +
-            "#{number - 1} bottles of beer on the wall.\n"
+            "#{number - 1} #{container(number)} of beer on the wall.\n"
         end
     end
 
-    def verses(upper, lower)
-        upper.downto(lower).collect { |i| verse(i) }.join("\n")
-    end
-
-    def song
-        verses(99,0)
+    def container(number=:FIXME)
+        if number == 2
+            "bottle"
+        else
+            "bottles"
+        end
     end
 end
